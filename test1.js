ed70df2bdeb99503e987339af7ef8f731c312b04
Luis Enrique Oviedo <le.oviedo@gmail.com>
2022-02-10 20:31:12 -0300

    Update test1.js
    final solution for the test.

--- test1.js
+++ test1.js
@@ -1,7 +1,3 @@
-
-
-
-
 // luisoviedo tt test1
 //
 /*
@@ -10,23 +6,29 @@ the values of coins in your possession, write a function that returns the
 minimum amount of change (the minimum sum of money) that you CANNOT create
 
 */
-coins = [5, 7, 1, 1, 2, 3, 22]
+
+coins = [5, 7, 1, 1, 2, 3, 22];
+coins1 = [1, 1, 1, 1, 1];
+coins2 = [1, 5, 1, 1, 1, 10, 15, 20, 100];
 
 function testcoins(coins) {
 
-    coins = coins.sort()
-
+    //sort the array with a lambda expression, key step for the solution.
+    coins = coins.sort((a,b) => a-b)
     
+    //console.log(coins);
+    //var for the max change possible
+    let max_sum = 0;
 
-    for (coin in coins) {
-        
-
+    //loop through the array
+    for (act_val of coins) {
+        //if the sum of previous coins: max_val plus 1 is greater than the actual coin we have, we keep summing until the less sum of coins is maxl_val +1
+        if (act_val > max_sum +1){ return max_sum+1}
+        max_sum += act_val; 
     }
-
+    return max_sum;
 }
 
-
-
-
-
-
+console.log(testcoins(coins))
+console.log(testcoins(coins1))
+console.log(testcoins(coins2))
