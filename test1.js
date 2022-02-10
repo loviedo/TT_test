563f81f4876c5aed97ebef35c2a03e24f8c6e597
Luis Enrique Oviedo <le.oviedo@gmail.com>
2022-02-10 19:25:09 -0300

    Create test1.js
    initial push

---
+++ test1.js
@@ -0,0 +1,32 @@
+
+
+
+
+// luisoviedo tt test1
+//
+/*
+Given an array of positive integers representing 
+the values of coins in your possession, write a function that returns the 
+minimum amount of change (the minimum sum of money) that you CANNOT create
+
+*/
+coins = [5, 7, 1, 1, 2, 3, 22]
+
+function testcoins(coins) {
+
+    coins = coins.sort()
+
+    
+
+    for (coin in coins) {
+        
+
+    }
+
+}
+
+
+
+
+
+
